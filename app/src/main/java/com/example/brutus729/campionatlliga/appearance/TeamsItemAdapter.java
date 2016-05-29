d39948b5e0adb7ce846241598700ddfe259b7592
adrian729 <adrian.sanchez.albanell@gmail.com>
2016-05-30 00:26:19 +0200

    afegint teams layout i llistes

---
+++ app/src/main/java/com/example/brutus729/campionatlliga/appearance/TeamsItemAdapter.java
@@ -0,0 +1,7 @@
+package com.example.brutus729.campionatlliga.appearance;
+
+/**
+ * Created by brutus729 on 30/05/16.
+ */
+public class TeamsItemAdapter {
+}
--- app/src/main/java/com/example/brutus729/campionatlliga/objects/TeamsItem.java
+++ app/src/main/java/com/example/brutus729/campionatlliga/objects/TeamsItem.java
@@ -4,5 +4,24 @@ package com.example.brutus729.campionatlliga.objects;
  * Created by brutus729 on 30/05/16.
  */
 public class TeamsItem {
-    
+
+
+    private String name;
+
+    public TeamsItem(){
+        super();
+    }
+
+    public TeamsItem(String team_name){
+        super();
+        this.name = team_name;
+    }
+
+    public String getTeamsName() {
+        return name;
+    }
+
+    public void setTeamsName(String team_name) {
+        this.name = team_name;
+    }
 }
